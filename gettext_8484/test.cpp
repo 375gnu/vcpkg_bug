3f472cff816de58164196fad6759b22534efdb3f
Hleb Valoshka <375gnu@gmail.com>
2019-10-08 21:26:34 +0300

    vcpkg bug with gettext #8484

---
+++ gettext_8484/test.cpp
@@ -0,0 +1,25 @@
+#include <libintl.h>
+#include <locale.h>
+#ifdef _MSC_VER
+#include <direct.h>
+#define getcwd(a, b) _getcwd(a, b)
+#else
+#include <unistd.h>
+#endif
+#include <iostream>
+
+int main()
+{
+
+    setlocale(LC_ALL, "");
+
+    char cwd[256];
+    getcwd(cwd, 255);
+
+    std::cout << bindtextdomain("test", cwd) << '\n';
+    std::cout << bind_textdomain_codeset("test", "UTF-8") << '\n';
+    std::cout << textdomain("test") << '\n';
+
+    std::cout << bind_textdomain_codeset("test", "CP1251") << '\n';
+    std::cout << gettext("test\n");
+}
